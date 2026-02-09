34ad6e9355800a7945289c3126b70630bca15e3e
Pin012 <husk9612@gmail.com>
2026-02-09 09:33:27 +0000

    Exit

--- views/GameView.tsx
+++ views/GameView.tsx
@@ -279,7 +279,7 @@ const GameView: React.FC<GameViewProps> = ({ game, players, currentPlayer, onExi
         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-zinc-400 group-hover:text-red-500 transition-colors" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
         </svg>
-        <span className="text-[11px] font-black uppercase tracking-[0.2em] text-zinc-300 group-hover:text-white transition-colors">退出房間</span>
+        <span className="text-[11px] font-black uppercase tracking-[0.2em] text-zinc-300 group-hover:text-white transition-colors">EXIT</span>
       </button>
 
       {showRoundBanner && (
